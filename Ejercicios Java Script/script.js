05275558c0b0d0b13db74a5b909a36ff0336d4b3
Santyyy06 <arboledacalzada323@gmail.com>
2025-05-03 12:21:19 -0500

    Add files via upload

---
+++ Ejercicios Java Script/script.js
@@ -0,0 +1,43 @@
+{console.log("Hola mundo en Java Script")
+
+
+
+
+var a = parseInt(prompt("Ingrese el valor de A"))
+
+var b = parseInt(prompt("ingrese el valor de B"))
+
+let suma = a+b
+
+const pi = Math.pi
+
+console.log ("El valor de pi es", pi)
+
+console.log ("La suma es", suma)
+
+var listaNumero = [ 12, 34, 56, 78, 90]
+console.log(" En la posicion 3 esta: ", listaNumero[ 3 ])
+
+
+var C = 1
+
+while (C <= 10)
+    console.log(C)
+}
+
+
+var contador = 1
+
+for(contador = 1; contador <= 10; contador++) {
+   console.log(contador)
+}
+
+
+//    Posiciones  [0    1   2   3   4]
+var arregloAdso =[ 12, 34, 56, 78, 90 ]
+
+var nombres = ["santiago","miguel","gabriel","ana","didier","daniel","jose","angie"]
+
+console.log("EL valor de la posicion 2", arregloAdso[ 2 ])
+
+console.log("EL valor de la posicion 4", nombres[ 2 ])
